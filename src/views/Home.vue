6782c990d8fa0ce96bcaba14f180d52679d82087
Teodora <aktr9886@protonmail.com>
2023-05-25 13:55:38 +0300

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { computed } from '@vue/reactivity';
+import store from '../store';
+
+const meals = computed(() => store.state.meals)
+</script>
+
+<template>
+  <pre>{{ meals }}</pre>
+</template>
